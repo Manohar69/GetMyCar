34ccb8cd3d4836f533bd8cd39f1b8d8045a86750
Manohar Nuthalapati <n.manohar123@gmail.com>
2019-10-30 21:19:43 +0530

    adding email poc

---
+++ SendEmail/Default.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="width:100%">
+    <div style="width:10%;float:left;">
+        To :<br />
+        Name:<br />
+        Email:<br />
+        Phone:<br />
+        Comments<br />
+        </div>
+        
+    <div style="width:20%;float:left;">
+        <asp:TextBox ID="txtToMail" runat="server"></asp:TextBox>
+        <br />
+        <asp:TextBox ID="txtName" runat="server"></asp:TextBox> 
+          <br />
+        <asp:TextBox ID="txtemail" runat="server"></asp:TextBox>
+        <br />
+         <asp:TextBox ID="txtPhone" runat="server"></asp:TextBox>
+        <br />
+        <asp:TextBox ID="check" runat="server"></asp:TextBox>
+        <br />
+        
+        
+    </div>
+       <div style="width:70%;float:right;"></div><br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ SendEmail/Default.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            //here on button click what will done 
+            SendMail();
+            Response.Redirect(Request.Url.PathAndQuery, true);
+            
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("exception");
+            Response.Write("Exception due to Network Problem");
+        }
+    }
+
+    protected void SendMail()
+    {
+        // Gmail Address from where you send the mail
+        var fromAddress = "rayudu.hebeon@gmail.com";
+        // any address where the email will be sending
+        var toAddress = txtToMail.Text;
+        //Password of your gmail address
+        const string fromPassword = "rayudu@123";
+        // Passing the values and make a email formate to display
+        string subject = "Sample Send Mail ";
+        string body = "From: " + txtName.Text + "\n";
+
+        body += "Email: " + txtemail.Text + "\n";
+        body += "PhoneNumber: " + txtPhone.Text + "\n";
+        
+        body += "Message: " + check.Text + "\n";
+        //for file uploading
+
+
+        // smtp settings
+        var smtp = new System.Net.Mail.SmtpClient();
+        {
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.EnableSsl = true;
+            smtp.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+            smtp.Credentials = new NetworkCredential(fromAddress, fromPassword);
+            smtp.Timeout = 20000;
+        }
+
+        // Passing values to smtp object
+        smtp.Send(fromAddress, toAddress, subject, body);
+        Response.Write("Message Sent Sucessfully");
+    }
+
+
+}
+
+ 
+
